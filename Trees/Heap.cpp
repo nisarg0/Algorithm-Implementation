d6e1be3901e618e57c7e1993be372946dc14f9a2
nisarg0 <nisarggogate212@gmail.com>
2021-07-22 01:00:00 +0530

    :tada: Heap sort added

---
+++ Trees/Heap.cpp
@@ -0,0 +1,59 @@
+// Given an array implementation of Complete Binary Tree
+// Heapify function takes O(logn) 
+// n is the size of array.
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+ 
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+ 
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+ 
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+ 
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+ 
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Now arr is a max heap
+    // Pass by ref
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+ 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main(){
+    int n = 5;
+    int arr[n] = {10,3,2,5,7};
+
+    heapSort(arr,n);
+
+    for(int i=0;i<n;i++)    
+        cout << arr[i] << " ";
+}
