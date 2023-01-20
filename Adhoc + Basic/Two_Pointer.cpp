726e94f1482fc3e2d434d2904b2fc79d3ede56c8
Nisarg Gogate <60577767+nisarg0@users.noreply.github.com>
2023-01-20 14:38:30 +0530

    Update Two_Pointer.cpp

--- Adhoc + Basic/Two_Pointer.cpp
+++ Adhoc + Basic/Two_Pointer.cpp
@@ -13,7 +13,7 @@ void sortColors(vector<int>& arr) {
     //
     while(mid <= hi) {
         if(arr[mid] == 0)
-            swap(arr[lo++],arr[mid++]);
+            swap(arr[lo++],arr[mid]);
         else if(arr[mid] == 1)
             mid++;
         else // arr[mid] = 2
