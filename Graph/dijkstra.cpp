97d54ba8b85f8f64a6fe0177f32cea48143b8336
Nisarg Gogate <60577767+nisarg0@users.noreply.github.com>
2024-05-15 22:45:08 +0530

    Update dijkstra.cpp

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,6 +1,6 @@
 /*
 Minimum distance of src from every other node present in graph with all non-negative weights
-Complexity for dijktra's Algo -> O(E+VlnV) 
+Complexity for dijktra's Algo -> O((V + E) log V)
 E-Edges V-Vertices
 
 Slight modification: Think of adding a virtual node which joins an edge to each node. [Similar to multi source BFS]
