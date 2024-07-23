7a456b37beb74c400edb470b2596a5d09fd81317
nisarg <nisarggogate212@gmail.com>
2024-07-23 10:18:18 +0530

    Refactored code and added explaination for bi partite matching.

--- Graph/Bipartite_matching.cpp
+++ Graph/Bipartite_matching.cpp
@@ -6,17 +6,18 @@
  *
  * @ref: https://www.geeksforgeeks.org/maximum-bipartite-matching/
  * @ref: https://cp-algorithms.com/graph/kuhn_maximum_bipartite_matching.html
+ * @ref: https://leetcode.com/problems/maximum-number-of-accepted-invitations/solutions/1978859/python-hungarian-algorithm-easy-to-understand/
  */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int n = 6, m = 6;
+int jobs = 6, applicants = 6;
 
 vector<vector<int> > G;
-vector<int> currentMatching(n, -1); // denotes ith job applicant mapped with currentMatch[i] job or -1 if no job
-vector<bool> visited(n);            // Usual use so that dfs doesn't visit same vertex again
+vector<int> currentMatching(jobs, -1);       // Job i mapped with currentMatching[i] applicant.
+vector<bool> visited(applicants);            // Usual use so that dfs doesn't visit same vertex again
 
 /**
  * Main idea behind the logic is a job applicant will only leave job if he/she gets some other job.
@@ -24,16 +25,18 @@ vector<bool> visited(n);            // Usual use so that dfs doesn't visit same 
  * @return bool : true a new match is added
  */
 
-bool findMaxBiPartiteMatching(int u)
+bool findMaxBiPartiteMatching(int u) // applicant
 {
-    if (visited[u])
+    if (visited[u]) // if applicant is already assigned or checked no need to recheck
         return false;
 
     visited[u] = true;
 
+    // Checking if vth job if already selected by some applicant
     for (auto v : G[u]) {
         // if current matching is available for v or if v can find some other job
-        if ( currentMatching[v] == -1 || findMaxBiPartiteMatching(v) ) {
+        // checking if selected applicant for job v can find some other job
+        if ( currentMatching[v] == -1 || findMaxBiPartiteMatching(currentMatching[v]) ) {
             currentMatching[v] = u;
             return true;
         }
@@ -43,13 +46,20 @@ bool findMaxBiPartiteMatching(int u)
 
 int main()
 {
-    G = {{1, 2}, {0, 3}, {2}, {2, 3}, {}, {5}}; // job 0 has {1,2} selected applicants.
+    G = {
+            {1, 2}, // Applicant 0 has {1, 2} job offers
+            {0, 3}, // Applicant 1 has {0, 3} job offers 
+            {2}, 
+            {2, 3}, 
+            {}, 
+            {5}
+        };
 
     int ans = 0;
-    for (int job = 0; job < m; job++)
+    for (int applicant = 0; applicant < applicants; applicant++)
     {
-        visited.assign(n, false);
-        if(findMaxBiPartiteMatching(person))
+        visited.assign(applicants, false);
+        if(findMaxBiPartiteMatching(applicant))
             ans++;
     }
 
