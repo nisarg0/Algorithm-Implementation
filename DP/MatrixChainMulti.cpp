0225fef5bf2a6ed3d19afd81df0e649ec4aa23b0
Nisarg Gogate <60577767+nisarg0@users.noreply.github.com>
2024-07-25 18:05:02 +0530

    Update MatrixChainMulti.cpp

--- DP/MatrixChainMulti.cpp
+++ DP/MatrixChainMulti.cpp
@@ -1,2 +1,4 @@
 // We use this techinique when we have to put brackets in the given list based on some condn.
-// 
+// This solution is used when the order in which we pick a element determines the answer.
+
+// Very good que: https://leetcode.com/problems/burst-balloons/
